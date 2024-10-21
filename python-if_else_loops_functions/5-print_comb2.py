00cf2e213a3f9b8a79ecf4a9fac67b84fb8d2ec8
El-gibbor <chiagoziem11@yahoo.com>
2024-10-21 21:26:06 +0200

    prints numeric values from 1 to 100

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,5 +4,4 @@ for i in range(100):
     if i < 99:
         print('{}'.format(str(i).zfill(2)), end=', ')
     else:
-        print('{}'.format(str(i).zfill(2)), end='')
-print()
+        print('{}'.format(str(i).zfill(2)), end='\n')
