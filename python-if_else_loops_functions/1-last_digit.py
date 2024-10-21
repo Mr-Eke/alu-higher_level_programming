a60660ea987cf7d1870f13dcba2f21bbe9099e69
El-gibbor <chiagoziem11@yahoo.com>
2024-10-21 19:57:09 +0200

    getting the last digit of a number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 
 # Extract the last digit, using absolute value for negative numbers
-last_digit = abs(number) % 10
+last_num = abs(number) % 10
 
 if number < 0:
-    last_digit = -last_digit  # Preserve negative sign for negative numbers
+    last_num = -last_num  # Preserve negative sign for negative numbers
 
-if last_digit < 6 and last_digit != 0:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
-elif last_digit > 5:
-    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+if last_num < 6 and last_num != 0:
+    print(f'Last digit of {number} is {last_num} and is less than 6 and not 0')
+elif last_num > 5:
+    print(f'Last digit of {number} is {last_num} and is greater than 5')
 else:
-    print(f'Last digit of {number} is {last_digit} and is 0')
+    print(f'Last digit of {number} is {last_num} and is 0')
