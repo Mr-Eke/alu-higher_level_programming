289a5ce72667542386391eae11cf8ca2393421f9
El-gibbor <chiagoziem11@yahoo.com>
2024-10-22 20:46:42 +0200

    fizzbuz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-def fizbuzz():
+# def fizbuzz():
+#     for i in range(1, 101):
+#         if i % 3 == 0:
+#             print('Fizz', end=' ')
+#         elif i % 5 == 0:
+#             print('Buzz', end=' ')
+#         elif i % 15 == 0:
+#             print('FizzBuzz', end='')
+#         else:
+#             print(i, end=' ')
+def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
-            print('Fizz', end=' ')
-        elif 1 % 15 == 0:
-            print('FizzBuzz', end='')
-        else:
-            print(i, end=' ')
-fizbuzz()
+        fizzBuzz = "Fizz" * (not i % 3) + "Buzz" * (not i % 5)
+        if not fizzBuzz:
+            fizzBuzz = i
+        print('{}'.format(fizzBuzz), end=' ')
