ec2aaa0d6d333881e16a3ded415afcf7b0b0d867
El-gibbor <chiagoziem11@yahoo.com>
2024-10-22 20:08:18 +0200

    converts characters to uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char_str in range(len(str)):
-        char = ord(str[char_str])
+    for i in range(len(str)):
+        char = ord(str[i])
         if 97 <= char <= 122:
             char -= 32
         print('{}'.format(chr(char)), end='')
