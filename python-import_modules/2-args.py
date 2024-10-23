a270f535e0ad836e2c2654fa2a08c0854ccf2dbf
El-gibbor <chiagoziem11@yahoo.com>
2024-10-23 17:20:21 +0200

    exploring commandline argument

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    cmd_args = len(argv) - 1
+
+    if cmd_args == 0:
+        print("{} arguments.".format(cmd_args))
+    if cmd_args == 1:
+        print("{} arguments:".format(cmd_args))
+        print("{}: {}".format(cmd_args, argv[1]))
+
+    print("{} arguments:".format(cmd_args))
+    for indx, i in enumerate(argv[1:], start=1):
+        print("{}: {}".format(indx, i))
---
+++ python-import_modules/mm.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import sys
+
+for i in sys.argv:
+    print(i)
