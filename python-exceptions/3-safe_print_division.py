ead5955523ba230b64c31224398bf46c559e7630
Mr-Eke <c.eke@alustudent.com>
2024-10-30 15:09:02 +0200

    Exception: used the Finally block'

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div_result = 0
+    div_result = None
     try:
         div_result = a / b
     except ZeroDivisionError:
-        return None
+        pass
     else:
         return div_result
     finally:
         print("Inside result: {}".format(div_result))
+        return div_result
