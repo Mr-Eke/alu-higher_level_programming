a9881541a299832ee94353456e301dedea32e7d2
Mr-Eke <c.eke@alustudent.com>
2024-11-08 23:58:50 +0200

    practiced raising of exceptions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-
-def divide_elements(a, b):
-    try:
-        return a / b
-    except (ZeroDivisionError):
-        print('division by zero')
-        return 0
-    except (TypeError):
-        print('wrong type')
-        return 0
-
 def list_division(my_list_1, my_list_2, list_length):
-    return [
-        divide_elements(a, b)
-        for a, b in zip(my_list_1[:list_length], my_list_2[:list_length])
-    ]
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
 
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+    return (new_list)
