a3ed7b043904c656489021f26c3de70a6bdbd0e6
Mr-Eke <c.eke@alustudent.com>
2024-11-09 12:59:52 +0200

    reimplemented all tasks for inheritence

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
-""" This module creates a class, 'myList'
-    This module creates a class, 'myList'
-    This module creates a class, 'myList'
-"""
+""" a module showcasing a simple usecase of inheritance"""
 
 
 class MyList(list):
-    """
-    MyList class that inherits from the built-in list class and .
-    This class is designed to represent a list with an added met.
-    """
-
-    def __init__(self):
-        """
-        Initializes an instance of the MyList class.
-
-        Inherits from the built-in list class and does not require
-        """
-        super().__init__()  # Initialize the parent class (list)
+    """ defines a derived class of List. """
 
     def print_sorted(self):
-        """
-        Prints the elements of the list in ascending sorted order.
-
-        Uses Python's built-in sorted() function to sort ult
-        without modifying the original list order.
-        """
+        """ prints the list, but sorted """
         print(sorted(self))
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,1 @@
-# python-inheritance
+Welcome to the Pythonic side of things where we delve into a key OOP concept called - Inheritance
